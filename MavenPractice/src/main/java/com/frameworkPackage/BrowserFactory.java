2cff1b0f843fad21b319a51443c3dc82f662bde6
Sri <Sri@DESKTOP-NF0B2AQ>
2020-01-18 10:33:30 +0530

    test

---
+++ MavenPractice/src/main/java/com/frameworkPackage/BrowserFactory.java
@@ -0,0 +1,5 @@
+package com.frameworkPackage;
+
+public class BrowserFactory {
+
+}
--- MavenPractice/src/test/java/MavenDemo.java
+++ MavenPractice/src/test/java/MavenDemo.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MavenDemo 
@@ -10,9 +13,27 @@ public class MavenDemo
 		System.out.println("Hii Git, Hii Jenkins");
 		System.out.println("Testing Jenkins auto build");
 		System.out.println("Testing Jenkins auto build");
-System.out.println("ttttt");
+		System.out.println("ttttt");
+		System.out.println("Hi");
+		System.out.println("Hi Hi");
 		
 	}
 	
+	@Test
+
+	public void checkTite()
+	{
+
+			System.setProperty("webdriver.chrome.driver",
+					"E://Selenium Software//Java_Selenum_Setup_Installer//selenium-java-3.0.0-beta3//chromedriver_win32//chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			driver.get("file:///E:/Java_Selenium_Final/Offline%20Website/offline/Offline%20Website/index.html");
+			driver.manage().window().maximize();
+			String ExpectedMsg = "JavaByKiran | Log in";
+			String ActMsg = driver.getTitle();
+			Assert.assertEquals(ActMsg, ExpectedMsg);
+			driver.quit();
+
+	}
 }
 	
